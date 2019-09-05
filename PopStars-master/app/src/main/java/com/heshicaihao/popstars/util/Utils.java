53ee7317c85b61a58a6b8da5ad32bd4d9f5df5ff
Heshicaihao <80004048@sfmail.sf-express.com>
2019-09-05 11:54:14 +0800

    xx

--- PopStars-master/app/src/main/java/com/heshicaihao/popstars/util/Utils.java
+++ PopStars-master/app/src/main/java/com/heshicaihao/popstars/util/Utils.java
@@ -79,42 +79,6 @@ public class Utils {
 
 	private static final String dbNmae = "clearStar";
 
-	/* @author sichard
-    * @category 判断是否有外网连接（普通方法不能判断外网的网络是否连接，比如连接上局域网）
-    * @return
-    */ 
-   public static final boolean ping() { 
-    
-       String result = null; 
-       try { 
-               String ip = "www.baidu.com";// ping 的地址，可以换成任何一种可靠的外网 
-               Process p = Runtime.getRuntime().exec("ping -c 3 -w 100 " + ip);// ping网址3次 
-               // 读取ping的内容，可以不加 
-               InputStream input = p.getInputStream(); 
-               BufferedReader in = new BufferedReader(new InputStreamReader(input)); 
-               StringBuffer stringBuffer = new StringBuffer(); 
-               String content = ""; 
-               while ((content = in.readLine()) != null) { 
-                       stringBuffer.append(content); 
-               } 
-               Log.d("ping", "result content : " + stringBuffer.toString()); 
-               // ping的状态 
-               int status = p.waitFor(); 
-               if (status == 0) { 
-                       result = "success"; 
-                       return true; 
-               } else { 
-                       result = "failed"; 
-               } 
-       } catch (IOException e) { 
-               result = "IOException"; 
-       } catch (InterruptedException e) { 
-               result = "InterruptedException"; 
-       } finally { 
-               Log.d("----result---", "result = " + result); 
-       } 
-       return false;
-	}
 
 	/**
 	 * 
